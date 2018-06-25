81beb451960243b3d459d6b2bff0855f7f580042
Joe Schafer <joeschaferastula@gmail.com>
2018-06-25 11:11:05 -0500

    save file working

--- deck.go
+++ deck.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-  "fmt"
-  "strings"
+	"fmt"
+	"io/ioutil"
+	"strings"
 )
 
 // Create a new type of 'deck'
@@ -32,9 +33,13 @@ func (d deck) print() {
 }
 
 func deal(d deck, handSize int) (deck, deck) {
-  return d[:handSize], d[handSize:]
+	return d[:handSize], d[handSize:]
 }
 
 func (d deck) toString() string {
-  return strings.Join([]string(d), ",")
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
