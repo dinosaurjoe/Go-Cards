00376322e88b79187fc342795e9874cb90c411c5
Joe Schafer <joeschaferastula@gmail.com>
2018-06-29 11:00:52 +0700

    embedded struct

--- structs/main.go
+++ structs/main.go
@@ -2,12 +2,24 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+  github string
+  zipCode int
+}
+
 type person struct {
   firstName string
   lastName string
+  contact contactInfo
 }
 
 func main() {
-  joe := person{firstName: "Joe", lastName:  "Schafer"}
-  fmt.Println(joe)
+  joe := person{
+    firstName: "Joe",
+    lastName: "Schafer",
+    contact: contactInfo{
+      github: "dinosaurjoe",
+      zipCode: 60622
+    }
+  }
 }
