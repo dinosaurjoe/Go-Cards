56114efdcc063b5eb5ad10c6d2d8779198fdf2bd
Joe Schafer <joeschaferastula@gmail.com>
2018-06-30 21:57:25 +0700

    struct receiver function

--- structs/main.go
+++ structs/main.go
@@ -10,16 +10,27 @@ type contactInfo struct {
 type person struct {
   firstName string
   lastName string
-  contact contactInfo
+  contactInfo
 }
 
 func main() {
   joe := person{
     firstName: "Joe",
     lastName: "Schafer",
-    contact: contactInfo{
+    contactInfo: contactInfo{
       github: "dinosaurjoe",
-      zipCode: 60622
-    }
+      zipCode: 60622,
+    },
   }
+
+  joe.updateName("Joseph")
+  joe.print()
+}
+
+func (p person) updateName(newFirstName string) {
+  p.firstName = newFirstName
+}
+
+func (p person) print() {
+  fmt.Printf("%+v", p)
 }
